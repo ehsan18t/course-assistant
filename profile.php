c63b95c9c803f6e870fd50efd7344cae3724d555
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-07 14:11:58 +0600

    redesigned login-page

--- profile.php
+++ profile.php
@@ -11,60 +11,62 @@
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
-	<meta charset="UTF-8">
-	<title>CSS User Profile Card</title>
-	<link rel="stylesheet" href="style.css">
-	<script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
+    <meta charset="UTF-8">
+    <title>CSS User Profile Card</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/b99e675b6e.js"></script>
 </head>
+
 <body>
 
-<div class="wrapper">
-    <div class="left">
-        <img src="img\avatar.png"
-        alt="user" width="100">
-        <h4><?php echo $user_data['f_name']." ".$user_data['l_name'];?></h4>
-    </div>
-    <div class="right">
-        <div class="info">
-            <h3>Information</h3>
-            <div class="info_data">
-                 <div class="data">
-                    <h4>Email : </h4>
-                    <p><?php echo $user_data['user_name'];?></p>
-                    <br>
-                    <h4>Phone</h4>
-                    <p><?php echo "NOT GIVEN";?></p>
-                 </div>
-                
+    <div class="wrapper">
+        <div class="left">
+            <img src="img\avatar.png" alt="user" width="100">
+            <h4><?php echo $user_data['f_name']." ".$user_data['l_name'];?></h4>
+        </div>
+        <div class="right">
+            <div class="info">
+                <h3>Information</h3>
+                <div class="info_data">
+                    <div class="data">
+                        <h4>Email : </h4>
+                        <p><?php echo $user_data['user_name'];?></p>
+                        <br>
+                        <h4>Phone</h4>
+                        <p><?php echo "NOT GIVEN";?></p>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="projects">
+                <h3>University</h3>
+                <div class="projects_data">
+                    <div class="data">
+                        <h4>Name : </h4>
+                        <p><?php echo $user_data['u_name'];?></p>
+                    </div>
+                    <div class="data">
+                        <h4>Department</h4>
+                        <p><?php echo $user_data['d_name'];?></p>
+                    </div>
+                </div>
+            </div>
+
+            <div class="social_media">
+                <ul>
+                    <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
+                    <li><a href="#"><i class="fab fa-twitter"></i></a></li>
+                    <li><a href="#"><i class="fab fa-instagram"></i></a></li>
+                </ul>
             </div>
         </div>
-      
-      <div class="projects">
-            <h3>University</h3>
-            <div class="projects_data">
-                 <div class="data">
-                    <h4>Name : </h4>
-                    <p><?php echo $user_data['u_name'];?></p>
-                 </div>
-                 <div class="data">
-                   <h4>Department</h4>
-                    <p><?php echo $user_data['d_name'];?></p>
-              </div>
-            </div>
-        </div>
-      
-        <div class="social_media">
-            <ul>
-              <li><a href="#"><i class="fab fa-facebook-f"></i></a></li>
-              <li><a href="#"><i class="fab fa-twitter"></i></a></li>
-              <li><a href="#"><i class="fab fa-instagram"></i></a></li>
-          </ul>
-      </div>
     </div>
-</div>
 
 </body>
+
 </html>
 
 
