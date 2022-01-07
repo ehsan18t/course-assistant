4d718f1f57b5d8b013c27ca837413d18b76f705f
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-07 19:11:50 +0600

    added custom header to all page

---
+++ template/nav-menu-for-logged-user.php
@@ -0,0 +1,28 @@
+<div class="nav-menu-container">
+    <div class="nav-menu-wrapper">
+        <a href="<?php echo HOME_PAGE; ?>" class="nav-menu-item mr-4c">
+            Home
+        </a>
+        <a href="<?php echo PROFILE_PAGE; ?>" class="nav-menu-item mr-4c">
+            Profile
+        </a>
+        <a href="#" class="nav-menu-item">
+            Course
+        </a>
+    </div>
+
+    <div class="search-bar">
+        <div class="search-bar-container">
+            <input type="text" class="search-bar-input" placeholder="Search anything...">
+            <div class="search-btn-size">
+                <input type="button">
+                <i class="fa fa-search search-btn-icon"></i>
+                </input>
+            </div>
+        </div>
+        <div class="nav-menu-container-right">
+            <a href="<?php echo INC_LOGOUT; ?>" class="nav-menu-btn">Logout</a>
+        </div>
+    </div>
+</div>
+</nav>
