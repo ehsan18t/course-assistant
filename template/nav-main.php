63b4a84611904ad8b2968e915ac360494436ec85
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-08 23:58:24 +0600

    moved menu button to logged user's header (Mobile View)

--- template/nav-main.php
+++ template/nav-main.php
@@ -6,11 +6,3 @@
         </svg>
         <span class="nav-logo-text">Course Assistant</span>
     </div>
-    <div class="nav-menu-icon-container">
-        <button class="nav-menu-icon">
-            <svg class="nav-menu-icon-size" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
-            </svg>
-        </button>
-    </div>
--- template/nav-menu-for-logged-user.php
+++ template/nav-menu-for-logged-user.php
@@ -1,3 +1,12 @@
+<div class="nav-menu-icon-container">
+    <button class="nav-menu-icon">
+        <svg class="nav-menu-icon-size" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
+            <title>Menu</title>
+            <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
+        </svg>
+    </button>
+</div>
+
 <div class="nav-menu-container">
     <div class="nav-menu-wrapper">
         <a href="<?php echo HOME_PAGE; ?>" class="nav-menu-item mr-4c">
