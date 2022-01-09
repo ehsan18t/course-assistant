fb8404b55b28ab27c82cedc4a7122b94401ddeca
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-10 01:23:58 +0600

    updated DBMS design

--- db/course_assistant.sql
+++ db/course_assistant.sql
@@ -1,62 +1,65 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Host: 127.0.0.1
--- Generation Time: Jan 06, 2022 at 04:19 PM
--- Server version: 10.4.21-MariaDB
--- PHP Version: 7.4.25
+CREATE TABLE users (
+    f_name varchar(30) NOT NULL,
+    l_name varchar(30) NOT NULL,
+    university varchar(100) NOT NULL,
+    department varchar(100) NOT NULL,
+    email varchar(50) NOT NULL,
+    u_id INT NOT NULL UNIQUE AUTO_INCREMENT,
+    domain varchar(20) NOT NULL,
+    password varchar(100) NOT NULL,
+    date timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+    CONSTRAINT pk_users PRIMARY KEY(email)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+--  Relational Table -- (users - trimesters)
+CREATE TABLE users_creates_trimesters (
+    email varchar(50),
+    t_id INT,
+    CONSTRAINT fk_users FOREIGN KEY (email) REFERENCES users(email),
+    CONSTRAINT fk_trimesters FOREIGN KEY (t_id) REFERENCES trimesters(t_id)
+);
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE trimesters (
+    t_id INT NOT NULL,
+    t_name varchar(20) NOT NULL,
+    is_running BOOLEAN NOT NULL,
+    start_date timestamp,
+    end_date timestamp,
+    fees decimal(8, 2),
+    scholarship INT,
+    CONSTRAINT pk_trimesters PRIMARY KEY (t_id)
+);
 
---
--- Database: `course_assistant`
---
+CREATE TABLE courses (
+    c_id INT NOT NULL,
+    c_name varchar(20) NOT NULL,
+    credit INT NOT NULL,
+    section varchar(2),
+    auto_add_to_group BOOLEAN NOT NULL,
+    expected_marks decimal(5, 2),
+    total_marks decimal(5, 2),
+    obtained_marks decimal(5, 2),
+    CONSTRAINT pk_courses PRIMARY KEY (c_id)
+);
 
--- --------------------------------------------------------
+--  Relational Table -- (trimester - courses)
+CREATE TABLE trimesters_has_courses (
+    email varchar(50),
+    c_id INT,
+    t_id INT,
+    CONSTRAINT fk_users FOREIGN KEY (email) REFERENCES users(email),
+    CONSTRAINT fk_trimesters FOREIGN KEY (t_id) REFERENCES trimesters(t_id),
+    CONSTRAINT fk_courses FOREIGN KEY (c_id) REFERENCES courses(c_id)
+);
 
---
--- Table structure for table `users`
---
-
-CREATE TABLE `users` (
-  `f_name` varchar(30) NOT NULL,
-  `l_name` varchar(30) NOT NULL,
-  `u_name` varchar(100) NOT NULL,
-  `d_name` varchar(100) NOT NULL,
-  `user_name` varchar(50) NOT NULL,
-  `domain` varchar(20) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `date` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `users`
---
-
-INSERT INTO `users` (`f_name`, `l_name`, `u_name`, `d_name`, `user_name`, `domain`, `password`, `date`) VALUES
-('Demo', 'User', 'United International University', 'Computer Science Engineering ', 'demoUser@bscse.uiu.ac.bd', 'bscse.uiu.ac.bd', '123456', '2022-01-06 15:17:40'),
-('Sunny', 'Sutradhar', 'United International University', 'Computer Science Engineering ', 'ssutradhar201084@bscse.uiu.ac.bd', 'bscse.uiu.ac.bd', '123456789', '2022-01-06 08:12:40');
-
---
--- Indexes for dumped tables
---
-
---
--- Indexes for table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`user_name`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+CREATE TABLE assessments (
+    assess_id INT NOT NULL,
+    expected_marks decimal(5, 2),
+    total_marks decimal(5, 2),
+    obtained_marks decimal(5, 2),
+    type varchar(15) NOT NULL,
+    -- how many asses will happen
+    count INT,
+    CONSTRAINT pk_assessment PRIMARY KEY (assess_id)
+);
