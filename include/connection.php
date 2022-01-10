b6297ff7dfa7bcfec5eea73471d82fbd27a0f8c9
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-10 21:48:04 +0600

    improved db connection with oop

--- include/connection.php
+++ include/connection.php
@@ -1,4 +1,18 @@
 <?php
-    if (!$con = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME)) {
-        die("failed to connect!");
+// Connect to MySQL
+    $conn = new mysqli(DB_HOST, DB_USER, DB_PASS);
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+
+    // If database is exists select it
+    if (!mysqli_select_db($conn, DB_NAME)){
+        // id not exists create one
+//        $sql = "CREATE DATABASE ".DB_NAME;
+//        if ($conn->query($sql) === TRUE) {
+//            echo "Database created successfully";
+//        } else {
+//            echo "Error creating database: " . $conn->error;
+//        }
+        die("Connection failed: " . $conn->connect_error);
     }
