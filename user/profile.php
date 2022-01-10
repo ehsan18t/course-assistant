18b981c861f190aede38024829dd017dce6ff2c3
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-10 21:52:37 +0600

    improved login and signup with new connection object

--- user/profile.php
+++ user/profile.php
@@ -4,7 +4,7 @@
 <?php
 require_once '../header.php';
 //if user is already login then this index page will be shown in browser
-$user_data = check_login($con);
+$user_data = check_login($conn);
 ?>
 
     <title>Home Page</title>
