f4d2f618afe35cf32acef6569cd37ea500c385bb
Ahsan Khan <nobody.ahsan@gmail.com>
2022-01-11 16:37:03 +0600

    removed deprecated configs

--- config/pages.php
+++ config/pages.php
@@ -11,20 +11,25 @@
     }
 
     // Link / Pages
-    const HOME_PAGE = DOMAIN . '/index.php';
-    const LOGIN_PAGE = DOMAIN . '/login.php';
-    const SIGNUP_PAGE = DOMAIN . '/signup.php';
-    const PROFILE_PAGE = DOMAIN . '/user/profile.php';
-    //const CHANGE_PASSWORD_PAGE = DOMAIN . '/change_password.php'; // not used
+    const PAGES = array(
+        'home' => DOMAIN . '/index.php',
+        'login' => DOMAIN . '/login.php',
+        'signup' => DOMAIN . '/signup.php',
+        'profile' => DOMAIN . '/user/profile.php',
+        'edit-profile' =>  DOMAIN . '/user/edit-profile.php',
+        'logout' => DOMAIN . '/user/logout.php'
+    );
 
     // Includes
-    const INC_CONNECTION = INC_DOMAIN . '/include/connection.php';
-    const INC_FUNCTION = INC_DOMAIN . '/include/function.php';
-    const INC_SIGNUP_FUNCTION = INC_DOMAIN . '/include/signup_function.php';
-    const INC_LOGOUT = DOMAIN . '/user/logout.php';
-    const TEM_NAV_MAIN = INC_DOMAIN . '/template/nav-main.php';
-    const TEM_NAV_LOGGED = INC_DOMAIN . '/template/nav-menu-for-logged-user.php';
-    const TEM_NAV_NON_LOGGED = INC_DOMAIN . '/template/nav-menu-for-non-logged-user.php';
+    const INCLUDES = array(
+        'connection' => INC_DOMAIN . '/include/connection.php',
+        'main-function' => INC_DOMAIN . '/include/function.php',
+        'signup-function' => INC_DOMAIN . '/include/signup_function.php',
+        'edit-profile-function' => INC_DOMAIN . '/include/edit-profile-function.php',
+        'nav-main-template' => INC_DOMAIN . '/template/nav-main.php',
+        'nav-logged-template' => INC_DOMAIN . '/template/nav-menu-for-logged-user.php',
+        'nav-non-logged-template' => INC_DOMAIN . '/template/nav-menu-for-non-logged-user.php'
+    );
 
     // Resources
     const CSS = array(
